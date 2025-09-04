b9ec26fe7fd933bdc975c8f2c08cd1a39703ab2c
koojawon <jawonkoo@naver.com>
2025-09-04 23:26:33 +0900

    feat: 최초 프로젝트 생성
    - 백엔드 프로젝트를 생성하였습니다.

---
+++ backend/src/main/java/io/ssafy/cinemoa/CinemoaApplication.java
@@ -0,0 +1,13 @@
+package io.ssafy.cinemoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemoaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/ssafy/cinemoa/CinemoaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ssafy.cinemoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
