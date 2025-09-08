088c0f102e534de7d9610b366b6e22b5b53f3d49
daeyeonlee <angellee98@naver.com>
2025-09-08 14:00:03 +0900

    feat : Next.js 프로젝트 생성
    - Tailwind CSS 적용
    - 기본 색상 적용
    - 폰트 적용
    - 로고 적용

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "씨네모아",
+  description: "씨네모아",
+  icons: {
+    icon: "/cinemoa_logo_short.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className="antialiased">{children}</body>
+    </html>
+  );
+}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-slate-900">Hello World</h1>
+      <span className="text-green-600 font-bold">초록색 텍스트</span>
+    </div>
+  );
+}
