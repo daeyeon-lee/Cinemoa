577ea78b822cfbb9046a87aed085ad058b9f9d54
daeyeonlee <angellee98@naver.com>
2025-09-09 10:21:52 +0900

    style : 색깔 컴포넌트 추가

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,12 +9,6 @@ module.exports = {
         'led-counter-7': ['LED Counter 7', 'sans-serif'],
       },
       colors: {
-        primary: '#f8fafc', // slate50
-        secondary: '#cbd5e1', // slate300
-        tertiary: '#94a3b8', // slate400
-        subtle: '#64748b', // slate500
-        inverse: '#0f172a', // slate900
-
         'Brand1-Primary': '#f53b3b',
         'Brand1-Secondary': '#d43333',
         'Brand1-Tertiary': '#a62828',
@@ -25,11 +19,27 @@ module.exports = {
         'Brand2-Tertiary': '#167873',
         'Brand2-Strong': '#71E5DE',
 
+        primary: '#f8fafc', // slate50
+        secondary: '#cbd5e1', // slate300
+        tertiary: '#94a3b8', // slate400
+        subtle: '#64748b', // slate500
+        inverse: '#0f172a', // slate900
+
         'BG-0': '#0f172a',
         'BG-1': '#1e293b',
         'BG-2': '#334155',
         'BG-3': '#475569',
         'BG-4': '#64748b',
+        'BG-Inverse': '#F8FAFC',
+        'BG-Dimmed': '#000000', // 투명도 설정정
+
+        'stroke-1': '#94A3B8',
+        'stroke-2': '#64748b',
+        'stroke-3': '#475569',
+        'stroke-4': '#0F172A',
+        'stroke-subtle': '#94A3B8',
+        'stroke-Strong': '#F8FAFC',
+        'stroke-Inverse': '#0F172A',
       },
     },
   },
