d28202271d0f1c6919c0bf75d7612cd413fd9b34
koojawon <jawonkoo@naver.com>
2025-09-11 11:29:12 +0900

    fix: 잘못된 PK 수정

--- backend/src/main/java/io/ssafy/cinemoa/funding/repository/entity/FundingStat.java
+++ backend/src/main/java/io/ssafy/cinemoa/funding/repository/entity/FundingStat.java
@@ -3,6 +3,8 @@ package io.ssafy.cinemoa.funding.repository.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -24,8 +26,11 @@ import org.hibernate.proxy.HibernateProxy;
 @AllArgsConstructor
 @Table(name = "funding_stats")
 public class FundingStat {
-
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "stat_id", nullable = false)
+    private Long statId;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "funding_id")
     private Funding funding;
