98037dc2314692fe5db6d8f4d6fe00116ca19542
이희경 <yhg9801@naver.com>
2025-09-11 16:10:00 +0900

    feat: 금융망 API 카드결제 클라이언트 구현

--- backend/src/main/java/io/ssafy/cinemoa/external/finance/dto/CreditCardTransactionResponse.java
+++ backend/src/main/java/io/ssafy/cinemoa/external/finance/dto/CreditCardTransactionResponse.java
@@ -4,6 +4,11 @@ import lombok.Data;
 
 @Data
 public class CreditCardTransactionResponse {
+    // 응답 헤더 정보
+    private String responseCode;
+    private String responseMessage;
+
+    // 거래 정보
     private String transactionUniqueNo;
     private String categoryId;
     private String categoryName;
