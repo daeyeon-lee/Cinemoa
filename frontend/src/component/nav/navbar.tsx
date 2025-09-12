53039c45a9b06aad46c8c3e44782dce20f3611b4
ijioo <easywoo980902@gmail.com>
2025-09-12 16:36:27 +0900

    feat: 로고클릭 시 /home 경로로 이동

--- frontend/src/component/nav/navbar.tsx
+++ frontend/src/component/nav/navbar.tsx
@@ -32,7 +32,9 @@ export default function Navbar() {
       <div className="border-b border-1 border-[#1E293B] lg:hidden">
         {/* 첫 번째 줄: 로고 + 아이콘들 */}
         <div className="flex items-center justify-between mb-4">
-          <img src="/cinemoa_logo_long.png" alt="씨네모아" className="h-8" />
+          <Link href="/home" className="cursor-pointer">
+            <img src="/cinemoa_logo_long.png" alt="씨네모아" className="h-8" />
+          </Link>
           <div className="flex items-center space-x-4">
             <SearchIcon />
             <UserIcon />
@@ -62,8 +64,9 @@ export default function Navbar() {
       {/* 데스크톱 레이아웃 - 한 줄 */}
       <div className="hidden lg:flex items-center justify-between">
         <div className="flex items-start justify-center gap-16 h-full">
-          {/* 로고고 */}
-          <Link href="/" className="cursor-pointer">
+          {/* 로고-홈으로 이동 */}
+          {/* 홈 주소로 변경함 */}
+          <Link href="/home" className="cursor-pointer">
             <img src="/cinemoa_logo_long.png" alt="씨네모아" className="w-[119px] h-full pb-1" />
           </Link>
           {/* navbar */}
