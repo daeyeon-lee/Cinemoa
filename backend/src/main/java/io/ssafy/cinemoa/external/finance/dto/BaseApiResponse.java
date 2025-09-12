8e95744507c7688a35f3135d24e69a8e500a3cbe
이희경 <yhg9801@naver.com>
2025-09-12 13:29:57 +0900

    feat: 금융망 API 요청, 응답 형식 맞춤

--- backend/src/main/java/io/ssafy/cinemoa/external/finance/dto/BaseApiResponse.java
+++ backend/src/main/java/io/ssafy/cinemoa/external/finance/dto/BaseApiResponse.java
@@ -1,9 +1,12 @@
 package io.ssafy.cinemoa.external.finance.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class BaseApiResponse<T> {
+  @JsonProperty("Header")
   private ResHeader Header; // 공통 헤더
+  @JsonProperty("REC")
   private T REC; // API별 고유 데이터
 }
