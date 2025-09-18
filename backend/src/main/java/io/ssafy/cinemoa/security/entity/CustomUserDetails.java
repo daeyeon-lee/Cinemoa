ecc3cbb82f9a420888b8b1529debdf5936e1188c
koojawon <jawonkoo@naver.com>
2025-09-18 13:13:01 +0900

    fix: 권핸 매핑 수정

--- backend/src/main/java/io/ssafy/cinemoa/security/entity/CustomUserDetails.java
+++ backend/src/main/java/io/ssafy/cinemoa/security/entity/CustomUserDetails.java
@@ -33,7 +33,7 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.getRole()));
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.getRole()));
     }
 
     @Override
