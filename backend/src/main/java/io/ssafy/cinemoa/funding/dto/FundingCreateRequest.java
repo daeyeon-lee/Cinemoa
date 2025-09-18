e3085cad3e4c0cc3e79453ab32f21cdee330f84b
koojawon <jawonkoo@naver.com>
2025-09-18 13:36:36 +0900

    fix: 타입 변경

--- backend/src/main/java/io/ssafy/cinemoa/funding/dto/FundingCreateRequest.java
+++ backend/src/main/java/io/ssafy/cinemoa/funding/dto/FundingCreateRequest.java
@@ -11,14 +11,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class FundingCreateRequest {
-    private long userId;
+    private Long userId;
     private String title;
     private String content;
-    private long categoryId;
+    private Long categoryId;
     private String videoName;
     private String posterUrl;
-    private long cinemaId;
-    private long screenId;
+    private Long cinemaId;
+    private Long screenId;
     private LocalDate screenDay;
     private byte screenStartsOn;
     private byte screenEndsOn;
