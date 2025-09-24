67f46ead321e925b7a982ca1c70466150e46de7f
daeyeonlee <angellee98@naver.com>
2025-09-24 17:26:10 +0900

    style : 푸터 스타일 수정

--- frontend/src/component/footer/footer.tsx
+++ frontend/src/component/footer/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
   const isDetailPage = pathname.startsWith('/detail');
 
   return (
-    <footer className={`bg-BG-1 px-4 py-10 mt-10 ${isDetailPage ? 'pb-[160px]' : 'pb-10'}`}>
+    <footer className={`bg-BG-1 px-4 py-10 mt-10 ${isDetailPage ? 'pb-[160px] md:pb-10' : 'pb-10'}`}>
       <div className="flex flex-col items-center justify-center gap-2">
         <p className="p3-b text-tertiary">SSAFY Cinemoa 프로젝트</p>
         <p className="caption1 text-tertiary">이용약관 | 개인정보 처리방침 | 판매 및 환불 | 법적 고지</p>
