af0ed53541a9f603a9842b7c97d2a3d8fcdb3316
daeyeonlee <angellee98@naver.com>
2025-09-25 14:07:13 +0900

    fix : CORS 오류 해결

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,6 +6,21 @@ const nextConfig: NextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   reactStrictMode: false, // StrictMode 비활성화
+
+  // COOP 정책 설정으로 소셜 로그인 팝업 통신 허용
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
