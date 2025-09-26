bfcd118e4319f137f221c98dfef8d6d8605eee55
ijioo <easywoo980902@gmail.com>
2025-09-26 15:16:09 +0900

    feat: disabled 속성 추가

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -4,18 +4,18 @@ import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
 
-const buttonVariants = cva('inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-[6px] px-3 py-2 transition-all cursor-pointer', {
+const buttonVariants = cva('inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-[6px] px-3 py-2 transition-all cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed', {
   variants: {
     variant: {
-      // 색상만 정의
-      primary: 'bg-BG-Inverse text-inverse',
-      secondary: 'bg-BG-3 text-secondary hover:bg-BG-2',
-      tertiary: 'bg-BG-2 text-secondary',
-      subtle: 'bg-BG-1 text-tertiary',
-      brand1: 'bg-Brand1-Primary text-primary hover:bg-Brand1-Secondary',
-      brand2: 'bg-Brand2-Primary text-inverse hover:bg-Brand2-Secondary',
-      outline: 'text-tertiary hover:border-stroke-2 border-stroke-4 border-2',
-      ghost: 'bg-transparent text-inverse hover:bg-BG-2 hover:text-secondary',
+      // 색상만 정의 + disabled 상태 스타일 추가
+      primary: 'bg-BG-Inverse text-inverse disabled:bg-stroke-3 disabled:text-tertiary',
+      secondary: 'bg-BG-3 text-secondary hover:bg-BG-2 disabled:bg-stroke-3 disabled:text-secondary',
+      tertiary: 'bg-BG-2 text-secondary disabled:bg-stroke-4 disabled:text-subtle',
+      subtle: 'bg-BG-1 text-tertiary disabled:bg-stroke-3 disabled:text-secondary',
+      brand1: 'bg-Brand1-Primary text-primary hover:bg-Brand1-Secondary disabled:bg-stroke-3 disabled:text-secondary',
+      brand2: 'bg-Brand2-Primary text-inverse hover:bg-Brand2-Secondary disabled:bg-stroke-3 disabled:text-secondary',
+      outline: 'text-tertiary hover:border-stroke-2 border-stroke-4 border-2 disabled:bg-BG-0 disabled:border-stroke-4 disabled:text-subtle',
+      ghost: 'bg-transparent text-inverse hover:bg-BG-2 hover:text-secondary disabled:text-subtle',
     },
     size: {
       // 크기만 정의
